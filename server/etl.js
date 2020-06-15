16f365c4a6533f19b83d29ed846430e37dead4cc
Mickey Mullin <wolf@mickeymullin.com>
2020-06-15 17:37:10 -0400

    Initialized package.json and split out statuses

--- server/etl.js
+++ server/etl.js
@@ -1,7 +1,7 @@
-const generate = require('csv-generate/lib/sync')
+// const generate = require('csv-generate/lib/sync')
 const parse = require('csv-parse/lib/sync')
 const stringify = require('csv-stringify')
-const transform = require('stream-transform')
+// const transform = require('stream-transform')
 const fs = require('fs').promises
 const fsSync = require('fs')
 const path = require('path')
@@ -11,9 +11,19 @@ const util = require('util')
 const dir = __dirname
 const dataDir = path.join(__dirname, '../data')
 const outputDir = path.join(__dirname, '../static')
-const SHOP_FILE = path.join(dataDir, 'FSP-Shop-orders-2020_06_12_10_10_02.csv')
 
-let pf17
+const dataFile
+  = 'FSP-Shop-orders-2020_06_14_19_43_03.csv'
+  // = 'FSP-Shop-orders-2020_06_14_12_22_50.csv'
+  // = 'FSP-Shop-orders-2020_06_08_12_06_37.csv'
+  // = 'FSP-Shop-orders-2020_06_12_10_10_02.csv'
+
+const SHOP_FILE = path.join(dataDir, dataFile)
+
+let pf17 = []
+let lookups = {
+  status: []
+}
 let isOutput = false
 
 parseCsv()
@@ -26,11 +36,12 @@ async function parseCsv() {
     on_record: (record, { lines }) => {
       // line_items
       const lineItems = parseItems(record, 'line_items')
-      if (lineItems.name) {
-        record.lineItemName = lineItems.name
+      if (lineItems.id) {
         if (lineItems.name.startsWith('PorcFest 2020')) {
           record.category = 'PorcFest 2020'
         }
+        record.lineItemName = lineItems.name
+        record.lineItemQuantity = lineItems.quantity
       }
       record.lineItems = lineItems
 
@@ -42,6 +53,19 @@ async function parseCsv() {
 
       // refunds
       record.refundItems = parseItems(record, 'refunds')
+
+      // lookups: status
+      if (record.category === 'PorcFest 2020') {
+        let statusIdx = lookups.status.findIndex(s => s.name === record.status)
+        if (statusIdx === -1) {
+          lookups.status.push({ name: record.status, count: 1 })
+        } else {
+          lookups.status[statusIdx].count++
+        }
+        // if (!lookups.status.includes(record.status)) {
+        //   lookups.status.push(record.status)
+        // }
+      }
 
       if (isOutput) {
         console.log('lines:' + JSON.stringify(lines))
@@ -57,7 +81,7 @@ async function parseCsv() {
         // OUTPUT fields
         // console.log('fields:')
         // let props = Object.keys(record)
-        // // props.sort()
+        // props.sort()
         // console.log(props)
 
         isOutput = false
@@ -69,17 +93,41 @@ async function parseCsv() {
 
   pf17 = records.filter(r => r.category === 'PorcFest 2020')
 
+  const reg = pf17.filter(pf => (pf.status === 'processing' || pf.status === 'completed') && pf.lineItemQuantity > 1)
+  const care = pf17.filter(pf => pf.status === 'cancelled' || pf.status === 'refunded')
+  const foh = pf17.filter(pf => pf.status === 'failed' || pf.status === 'on-hold')
+
+  const pf17Count = pf17.reduce(regSum, 0)
+  const pf17RegCount = reg.reduce(regSum, 0)
+  const pf17CareCount = care.reduce(regSum, 0)
+  const pf17FohCount = foh.reduce(regSum, 0)
+
+  // sort lookups
+  Object.keys(lookups).forEach(key => {
+    lookups[key].sort()
+  })
+
     // OUTPUT PorcFest 17 count
-  console.log('pf17 count:')
-  console.log(pf17.length)
+    console.log('pf17 records: ' + pf17.length + ', count: ' + pf17Count)
+    console.log('pf17 reg: ' + reg.length + ', count: ' + pf17RegCount)
+    console.log('pf17 care: ' + care.length + ', count: ' + pf17CareCount)
+    console.log('pf17 foh: ' + foh.length + ', count: ' + pf17FohCount)
 
   const json = records.map( JSON.stringify ).join('\n')
 
   console.log('-==] parseCsv done [==-')
 
+  writeFile(lookups, 'lookups.js')
+  writeFile(pf17, 'pf17.js')
+  writeFile(reg, 'pf17-reg.js')
+  writeFile(care, 'pf17-care.js')
+  writeFile(foh, 'pf17-foh.js')
+
   // writeCsv()
   writeJso()
-  writeJson()
+  writeJson(pf17, 'pf17')
+  writeJson(reg, 'pf17-reg')
+  writeJson(care, 'pf17-care')
 }
 
 function parseItems(record, items_name) {
@@ -93,6 +141,12 @@ function parseItems(record, items_name) {
   })
 
   return items
+}
+
+const regSum = (acc, curr) => acc + curr.lineItemQuantity * 1
+
+function propSum(arr, prop) {
+  return arr.reduce((acc, curr) => acc + curr[prop], 0)
 }
 
 function writeCsv() {
@@ -153,7 +207,6 @@ function writeCsv() {
 }
 
 function writeFile(arr, filename, arrName) {
-  const outputName = arrName ? arrName : 'items'
   const outputFile = path.join(outputDir, filename)
 
   fsSync.writeFileSync(outputFile, util.inspect(arr, {
@@ -162,7 +215,16 @@ function writeFile(arr, filename, arrName) {
     maxArrayLength: Infinity
   }, 'utf-8'))
 
-  console.log('wrote ' + filename + ' with ' + arr.length + ' ' + outputName)
+  let count, outputName
+  if (Array.isArray(arr)) {
+    outputName = arrName ? arrName : 'items'
+    count = arr.length
+  } else {
+    outputName = 'properties'
+    count = Object.keys(arr).length
+  }
+
+  console.log('wrote ' + filename + ' with ' + count + ' ' + outputName)
 }
 
 function writeFileRaw(arr, filename, arrName) {
@@ -176,12 +238,17 @@ function writeFileRaw(arr, filename, arrName) {
 
 function writeJso() {
   writeFile(pf17, 'pf17.js')
+  writeFile(lookups, 'lookups.js')
 }
 
-function writeJson() {
+function writeJson(arr, filename) {
+  filename = filename || 'output'
+
   fsSync.writeFileSync(
-    path.join(outputDir, 'pf17.json'),
-    JSON.stringify(pf17),
+    path.join(outputDir, filename + '.json'),
+    JSON.stringify(arr),
     'utf-8'
   )
+
+  console.log('wrote ' + filename + '.json')
 }
